11c9b617efd7b066437497b19d1cc518410c1367
mnm353 <mnm353@gmail.com>
2019-07-09 11:28:31 -0600

    updating

--- add two arrays.js
+++ add two arrays.js
@@ -1,3 +1,19 @@
-// I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements. I'll appreciate for your help.
+// I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements.
+//I'll appreciate for your help.
 
 // P.S. Each array includes only integer numbers. Output is a number too.
+
+function sumOfAll(arr) {
+	var sum = 0
+	for (var i = 0; i < arr.length; i++) {
+		for (var i = j; j < arr[i].length; i++) {
+			sum += arr[i][j]
+		}
+	}
+	return sum
+}
+
+//best practice
+function sumArray(arr) {
+	return arr.reduce((t, e) => t.concat(e)).reduce((t, e) => t + e)
+}
